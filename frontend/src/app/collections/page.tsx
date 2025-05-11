ce4c225dda745edc974f4b33174122747fa58db3
Cafourek, Lukáš <cafoulu1@fel.cvut.cz>
2025-05-11 16:51:03 +0200

    implemented testing suggestions

--- frontend/src/app/collections/page.tsx
+++ frontend/src/app/collections/page.tsx
@@ -262,7 +262,7 @@ export default function Collections() {
             className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
             onClick={() => setIsModalOpen(true)}
           >
-            Create New Card Set
+            Create Card Set
           </button>
         </div>
 
